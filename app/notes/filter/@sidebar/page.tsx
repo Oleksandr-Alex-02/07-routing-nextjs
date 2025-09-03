ec962dbdfe856f7399bd192db3d223c9cc162143
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2025-09-03 18:56:13 +0300

    @modal

---
+++ app/notes/filter/@sidebar/page.tsx
@@ -0,0 +1,25 @@
+
+import css from './page.module.css'
+
+export const categories = [
+    "All",
+    "Work",
+    "Personal",
+    "Todo",
+    "Meeting",
+    "Shopping",
+]
+
+export default function Sidebar() {
+    return (
+        <ul className={css.menuList}>
+            {/* список тегів */}
+            {categories.map((tag) =>
+                <li key={tag} className={css.menuItem}>
+                    <a href={`/notes/filter/${tag}`} className={css.menuLink}>
+                        {tag}
+                    </a>
+                </li>)}
+        </ul>
+    )
+}
